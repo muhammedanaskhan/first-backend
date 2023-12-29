b612e1e3347bcad9b6907b15461f4e31e8b88428
muhammedanaskhan <muhammedanas5186@gmail.com>
2023-12-29 09:23:58 -0800

    adding multer middleware

--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -7,26 +7,17 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
-// a method which would file link as param
-// and upload it to cloudinary then unlink if success
-
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null
 
-        //upload to cloudinary
         const response = await cloudinary.uploader.upload(localFilePath,
             { resource_type: "auto" });
 
-        //file has been uploaded succesfuly
         console.log("file uploaded!")
     } catch (err) {
 
-        // in failure we're still confirmed that file 
-        // has been added to local storage, there's only
-        // upload issue, so we must remove from local
-
-        fs.unlinkSync(localFilePath) //reomve locally saved temp file
+        fs.unlinkSync(localFilePath) //remove locally saved temp file
         return null;
     }
 }
