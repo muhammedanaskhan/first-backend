c3a04e9db329ce82e29b20757d80e7ae12a04270
muhammedanaskhan <muhammedanas5186@gmail.com>
2023-12-30 01:44:20 -0800

    writing first router and controller

--- src/app.js
+++ src/app.js
@@ -20,4 +20,17 @@ app.use(express.urlencoded({
 
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+
+
+ 
+// import routes
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+ app.use("/api/v1/users", userRouter)
+
+
 export {app} 
