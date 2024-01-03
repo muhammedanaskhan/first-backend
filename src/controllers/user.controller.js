cf62e3013d2f03fb9f53d059befc549c1623c360
muhammedanaskhan <muhammedanas5186@gmail.com>
2024-01-03 12:14:45 -0800

    Add user controller functions for updatePassword, getCurrentUser, and  updataAccountDetails

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -222,4 +222,63 @@ const refreshAccessToken = asyncHandler( async(req, res) => {
 
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken }
+const changeCurrentPassword = asyncHandler( async(req, res) => {
+
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req?.user._id)
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Invalid Password")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave: false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changes successfully"))
+})
+
+const getCurrentUser = asyncHandler( async(req,res) => {
+    return res
+    .status(200)
+    .json(200, req.user, "Current user exported successfully")
+}) 
+
+const updateAccountDetails = asyncHandler (async(req, res) => {
+
+    const{fullName, email} = req.body;
+
+    if(!fullName || !email){
+        throw new ApiError(400, "All fields are requied")
+    }
+
+    // check if authenticated user is only changing the fields .... ????
+
+    const user = await User.findByIdAndUpdate
+        (
+            req?.user._id,
+            {
+                $set:{
+                    fullName,
+                    email
+                }
+            },
+            {new: true}
+        ).select("-password")
+    
+        return res
+        .status(200)
+        .json(new ApiResponse(200, user, "Details updated!"))
+})
+
+export { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken,
+    getCurrentUser,
+    updateAccountDetails
+}
